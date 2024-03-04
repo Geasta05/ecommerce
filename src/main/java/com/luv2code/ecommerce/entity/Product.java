bf01a79144cdb76ab96cebba193f72218dbcfaa0
Scott Smith <ssm2876@wgu.edu>
2024-03-04 00:25:52 -0800

    add annotations to Product class

--- src/main/java/com/luv2code/ecommerce/entity/Product.java
+++ src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -1,7 +1,6 @@
 package com.luv2code.ecommerce.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
@@ -9,15 +8,27 @@ import lombok.Data;
 @Data
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "sku")
     private String sku;
+    @Column(name = "name")
     private String name;
+    @Column(name = "description")
     private String description;
+    @Column(name = "unit-price")
     private BigDecimal unitPrice;
+    @Column(name = "image-url")
     private String imageUrl;
+    @Column(name = "active")
     private boolean active;
+    @Column(name = "units-in-stock")
     private int unitsInStock;
+    @Column(name = "date-created")
     private Date dateCreated;
+    @Column(name = "last-updated")
     private Date lastUpdated;
 
 
