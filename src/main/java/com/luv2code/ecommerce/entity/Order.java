88bfb7a60d2a4e675e2993c91929244a46a8a7d1
Scott Smith <ssm2876@wgu.edu>
2024-03-09 13:22:56 -0800

    Add order and orderitem entities

---
+++ src/main/java/com/luv2code/ecommerce/entity/Order.java
@@ -0,0 +1,38 @@
+package com.luv2code.ecommerce.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "order_tracking_number")
+    private String orderTrackingNumber;
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+    @Column(name = "total_quantity")
+    private int totalQuantity;
+    @Column(name = "status")
+    private String status;
+    @CreationTimestamp
+    @Column(name = "date_created")
+    private Date dateCreated;
+    @UpdateTimestamp
+    @Column(name = "last_updated")
+    private Date lastUpdated;
+
+}
